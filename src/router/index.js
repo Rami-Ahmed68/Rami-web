4ba5f74edd7357ce28fcb7100956357cc48bcf80
RAMIAHMED <ramiahmet68@gmail.com>
2024-05-11 08:02:24 -0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,4 @@
-import Vue from "vue";
-import Router from "vue-router";
-// import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import UpdateAccountViewvue from "../views/UpdateAccountView.vue";
 import LoginViewvue from "../views/LoginView.vue";
@@ -10,11 +8,10 @@ import WorksViewvue from "../views/WorksView.vue";
 import SkillsViewvue from "../views/SkillsView.vue";
 import NotFound from "../views/NotFound.vue";
 
-Vue.useAttrs(Router);
-
-const router = new Router({
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   mode : "history",
-    routes : [
+  routes : [
     {
       path: '/',
       name: 'home',
@@ -57,53 +54,5 @@ const router = new Router({
     }
   ],
 })
-
-
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   mode : "history",
-//   routes : [
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: HomeView
-//     },
-//     {
-//       path: '/upaccount',
-//       name: 'upaccount',
-//       component : UpdateAccountViewvue
-//     },
-//     {
-//       path : "/login",
-//       name : "login",
-//       component : LoginViewvue,
-//     },
-//     {
-//       path : "/dash",
-//       name : "dash",
-//       component : DashBordViewvue
-//     },
-//     {
-//       path : "/work/:id",
-//       name : "work",
-//       component : ShowWorkView
-//     },
-//     {
-//       path : "/works",
-//       name : "works",
-//       component : WorksViewvue
-//     },
-//     {
-//       path : "/skills",
-//       name : "skills",
-//       component : SkillsViewvue
-//     },
-//     {
-//       path : "/:pathMatch(.*)*",
-//       name : "NotFound",
-//       component : NotFound
-//     }
-//   ],
-// })
 
 export default router
