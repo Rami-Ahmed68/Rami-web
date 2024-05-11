76447e94084e4225717610b9724d06512a40bdec
RAMIAHMED <ramiahmet68@gmail.com>
2024-05-11 04:24:32 -0700

    dd

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
+// import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import UpdateAccountViewvue from "../views/UpdateAccountView.vue";
 import LoginViewvue from "../views/LoginView.vue";
@@ -8,10 +10,11 @@ import WorksViewvue from "../views/WorksView.vue";
 import SkillsViewvue from "../views/SkillsView.vue";
 import NotFound from "../views/NotFound.vue";
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+Vue.useAttrs(Router);
+
+const router = new Router({
   mode : "history",
-  routes : [
+    routes : [
     {
       path: '/',
       name: 'home',
@@ -54,5 +57,53 @@ const router = createRouter({
     }
   ],
 })
+
+
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   mode : "history",
+//   routes : [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView
+//     },
+//     {
+//       path: '/upaccount',
+//       name: 'upaccount',
+//       component : UpdateAccountViewvue
+//     },
+//     {
+//       path : "/login",
+//       name : "login",
+//       component : LoginViewvue,
+//     },
+//     {
+//       path : "/dash",
+//       name : "dash",
+//       component : DashBordViewvue
+//     },
+//     {
+//       path : "/work/:id",
+//       name : "work",
+//       component : ShowWorkView
+//     },
+//     {
+//       path : "/works",
+//       name : "works",
+//       component : WorksViewvue
+//     },
+//     {
+//       path : "/skills",
+//       name : "skills",
+//       component : SkillsViewvue
+//     },
+//     {
+//       path : "/:pathMatch(.*)*",
+//       name : "NotFound",
+//       component : NotFound
+//     }
+//   ],
+// })
 
 export default router
