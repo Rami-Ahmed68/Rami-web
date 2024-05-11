0b8499f1ce1631080449bfc686e84f63b282a07a
RAMIAHMED <ramiahmet68@gmail.com>
2024-05-11 03:43:49 -0700

    test

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+
+// var historyApiFallback = require('connect-history-api-fallback');
+// gulp.task('browser-sync', function () {
+//     return browserSync.init({
+//         open: true,
+//         port: 3001,
+//         startPath: 'index.html',
+//         notify: false,
+//         server: {
+//         baseDir: ['.'],
+//         middleware: [ historyApiFallback() ]
+//         }
+//     });
+// });
